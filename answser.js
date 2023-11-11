a95f66633dc2705cd2ac8799d86b361280ca64ac
rabhi alqadi <rabhialqadi22@gmail.com>
2023-11-11 13:26:38 -0800

    get even answered

---
+++ answser.js
@@ -0,0 +1,12 @@
+// Write a for loop that will log the numbers 1 through 20.
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+  }
+   console.log();
+
+// Write a for loop that will log only the even numbers in 0 through 200.
+for (let i = 0; i <= 200; i += 2) {
+    console.log(i);
+  }
+  console.log();
+
