ef63332eb398ab614fe223b957ee978f95f844f8
rabhi alqadi <rabhialqadi22@gmail.com>
2024-01-04 14:56:52 -0800

    drinks data

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require('path'); // Require the 'path' module
-const drinks = require('./models/drinks'); // Assuming 'drinks.js' is in the 'models' folder
+const path = require('path');
+const drinks = require('./models/drinks');
 
-// Set 'views' directory for your views
 app.set('views', path.join(__dirname, 'views'));
-// Set view engine as EJS
 app.set('view engine', 'ejs');
 
 app.get('/drinks', (req, res) => {
-  res.render('index'); // Render 'index.ejs' when '/drinks' is accessed
+  res.render('index', { drinks: drinks }); // Pass drinks data to index.ejs
 });
 
 const port = 3000;
