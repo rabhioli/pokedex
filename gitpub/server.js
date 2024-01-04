d7d9dab69b2a3ba164080b97099cd39265108773
rabhi alqadi <rabhialqadi22@gmail.com>
2024-01-04 14:52:04 -0800

    route created

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,13 +1,11 @@
-const express = require("express")
+const express = require('express');
+const app = express();
+const drinks = require('./models/drinks'); 
+app.get('/drinks', (req, res) => {
+  res.send(drinks);
+});
 
- const app = express()
- const port = 3000;
-
- 
- app.get('/', (req, res) => {
-    res.send('Welcome to the Gitpub App!');
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
