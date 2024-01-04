8f571a8dc26ae9292db71c1125321d52856a6f22
rabhi alqadi <rabhialqadi22@gmail.com>
2024-01-04 14:54:16 -0800

    index route created

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-const drinks = require('./models/drinks'); 
+const path = require('path'); // Require the 'path' module
+const drinks = require('./models/drinks'); // Assuming 'drinks.js' is in the 'models' folder
+
+// Set 'views' directory for your views
+app.set('views', path.join(__dirname, 'views'));
+// Set view engine as EJS
+app.set('view engine', 'ejs');
+
 app.get('/drinks', (req, res) => {
-  res.send(drinks);
+  res.render('index'); // Render 'index.ejs' when '/drinks' is accessed
 });
 
 const port = 3000;
